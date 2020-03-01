18ca06292e381678c4ea8d6617f7526f47cf5e36
Joshua Jones <joshuajones@trentu.ca>
2020-03-01 14:51:57 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeatureSliderComponent } from './components/feature-slider/feature-slider.component';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureSliderComponent,
+    ProductGridComponent,
+    BannerComponent,
+    BestSellerComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
